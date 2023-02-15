e88e789073891f75d52dc516892114434b4c7c25
Darya <118105872+daryatsyb@users.noreply.github.com>
2023-02-15 13:34:51 +0100

    feat: add js cod

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = new Set(bracketsConfig.map(([open]) => open));
+  const closingBrackets = new Set(bracketsConfig.map(([, close]) => close));
+  const matchingBrackets = Object.fromEntries(bracketsConfig);
+  
+  for (const bracket of str) {
+    if (openingBrackets.has(bracket)) {
+      if (matchingBrackets[bracket] !== bracket || !closingBrackets.has(bracket)) {
+        stack.push(bracket);
+      } else {
+        if (stack[stack.length - 1] === bracket) {
+          stack.pop();
+        } else {
+          stack.push(bracket);
+        }
+      }
+    } else if (closingBrackets.has(bracket)) {
+      const lastOpeningBracket = stack.pop();
+      if (matchingBrackets[lastOpeningBracket] !== bracket) {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
 }
